565e1cf694bc76d3b3db8161acd28e216e3b1125
Nguyen Manh Hung <HungNMHE187081@fpt.edu.vn>
2024-09-26 14:18:47 +0700

    Resolved merge conflict in AddUserServlet.java

--- src/java/controller/AddUserServlet.java
+++ src/java/controller/AddUserServlet.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import dal.ManagerUserDAO;
@@ -26,34 +25,37 @@ import model.Ward;
  * @author LENOVO
  */
 public class AddUserServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet AddUserServlet</title>");  
+            out.println("<title>Servlet AddUserServlet</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet AddUserServlet at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet AddUserServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -61,28 +63,29 @@ public class AddUserServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-   ManagerUserDAO dao = new ManagerUserDAO();
-   List<Provinces> provinces = dao.getAllProvinces();
-   request.setAttribute("provinces", provinces);
-   request.getRequestDispatcher("form-add-user").forward(request, response);
-   
-}
-    
+            throws ServletException, IOException {
+        ManagerUserDAO dao = new ManagerUserDAO();
+        List<Provinces> provinces = dao.getAllProvinces();
+        request.setAttribute("provinces", provinces);
+        request.getRequestDispatcher("form-add-user").forward(request, response);
 
+        // Chuyển tiếp đến trang form-add-user.jsp
+        request.getRequestDispatcher("form-add-user.jsp").forward(request, response);
 
-    /** 
+    }
+
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-         Users user = new Users();
+            throws ServletException, IOException {
+        Users user = new Users();
         user.setFirstName(request.getParameter("firstName"));
         user.setMiddleName(request.getParameter("middleName"));
         user.setLastName(request.getParameter("lastName"));
@@ -113,7 +116,7 @@ public class AddUserServlet extends HttpServlet {
         response.sendRedirect("success.jsp");
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
      * @return a String containing servlet description
      */
