dfe68018b69f17f35e6ad9a85ee14777b9b4c240
anlpvhe187047 <anlpvhe187047@fpt.edu.vn>
2024-10-27 16:03:46 +0700

    Presciption begin

---
+++ src/java/model/Prescription.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author User
+ */
+public class Prescription {
+    private int prescriptionID;
+    private int recordID;
+    private int medicineID;
+    private String dosage;
+    private String frequency;
+    private String duration;
+
+    public Prescription() {
+    }
+
+    public Prescription(int prescriptionID, int recordID, int medicineID, String dosage, String frequency, String duration) {
+        this.prescriptionID = prescriptionID;
+        this.recordID = recordID;
+        this.medicineID = medicineID;
+        this.dosage = dosage;
+        this.frequency = frequency;
+        this.duration = duration;
+    }
+
+    public int getPrescriptionID() {
+        return prescriptionID;
+    }
+
+    public void setPrescriptionID(int prescriptionID) {
+        this.prescriptionID = prescriptionID;
+    }
+
+    public int getRecordID() {
+        return recordID;
+    }
+
+    public void setRecordID(int recordID) {
+        this.recordID = recordID;
+    }
+
+    public int getMedicineID() {
+        return medicineID;
+    }
+
+    public void setMedicineID(int medicineID) {
+        this.medicineID = medicineID;
+    }
+
+    public String getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
+
+    public String getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(String frequency) {
+        this.frequency = frequency;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+    
+}
